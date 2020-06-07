ed9ca24d9eb1d87a3588594af9543a4010b69e9b
Andrew Kao <andykao1213@gmail.com>
2020-06-07 18:39:29 +0800

    Setup Eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,16 @@
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => console.log("Hello World"));
+app.get('/', () => console.log('Hello World'));
 
 mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
-db.on("error", (err) => console.error(err));
-db.on("open", () => {
-	app.listen(port, () => {
-		console.log(`Server Ready at ${port}`);
-	});
+db.on('error', (err) => console.error(err));
+db.on('open', () => {
+  app.listen(port, () => {
+    console.log(`Server Ready at ${port}`);
+  });
 });
